30af6c8eb84a6182483c12de7b59848ed8c8ed52
Matt Triano <matt.triano@gmail.com>
2017-05-31 13:00:02 -0500

    figure out the y axis issue

--- Assignments/proj7TrianoMatt/proj7TrianoMatt.R
+++ Assignments/proj7TrianoMatt/proj7TrianoMatt.R
@@ -11,18 +11,68 @@ print.Die <- function(theObject) {
 }
 
 ChlorReads <- function(self.id, self.name, self.gender, self.ldl, self.hdl, self.trigl) {
-  this <- list(id=self.id, name=self.name, gender=self.gender, ldl=self.ldl, hdl=self.ldl, trigl=self.trigl)
+  this <- list(id=self.id, name=self.name, gender=self.gender, ldl=self.ldl, hdl=self.hdl, trigl=self.trigl)
   class(this) <- "ChlorReads"
   return(this)
 }
 
+get.ldl <- function(self) {
+  ldl <- self$ldl
+  if (ldl >= 240) {
+    ldl_symb <- paste0(ldl, "*")
+  } else if (ldl >= 200) {
+    ldl_symb <- paste0(ldl, "+")
+  } else if (ldl >= 0) {
+    ldl_symb <- paste0(ldl, "")
+  } else {
+    ldl_symb <- "ILLEGAL LDL VALUE!"
+  }
+  return(ldl_symb)
+}
+
+get.hdl <- function(self) {
+  hdl <- self$hdl
+  if (hdl >= 60) {
+    hdl_symb <- paste0(hdl, " ")
+  } else if (hdl >= 40) {
+    hdl_symb <- paste0(hdl, "+")
+  } else if (hdl >= 0) {
+    hdl_symb <- paste0(hdl, "*")
+  } else {
+    hdl_symb <- "ILLEGAL HDL VALUE!"
+  }
+  return(hdl_symb)
+}
+
+get.trigl <- function(self) {
+  trig <- self$trigl
+  if (trig >= 200) {
+    trig_symb <- paste0(trig, "*")
+  } else if (trig >= 150) {
+    trig_symb <- paste0(trig, "+")
+  } else if (trig >= 0) {
+    trig_symb <- paste0(trig, "")
+  } else {
+    trig_symb <- "ILLEGAL TRIGLYCERIDE VALUE!"
+  }
+  return(trig_symb)
+}
+
+plot.ChlorReads <- function(self) {
+  counts <- c(self$ldl, self$hdl, self$trigl)
+  barplot(height=counts, ylim=c(0, max(counts)), names=c("LDL","HDL","Triglycerides"))
+}
+
 print.ChlorReads <- function(self) {
-  cat("ID:", self$id, ", Name:", self$name, ", Gender:", self$gender, "\nLDL:",
-      self$ldl, ", HDL:", self$hdl, ", Triglycerides:", self$trigl, "\n")
+  cat("ID:", self$id, " Name:", self$name, " Gender:", self$gender, "\nLDL:",
+      get.ldl(self), " HDL:", get.hdl(self), " Triglycerides:", get.trigl(self), "\n")
 }
 
 p1 <- ChlorReads(9876, "Virgil", "M", 248, 45, 148)
 print.ChlorReads(p1)
 print(p1)
+plot(p1)
+
+
 
 #stopped the 2nd to last lectura aropund 1:30:00
