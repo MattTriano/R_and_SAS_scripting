42618ec063d0bb1092176d6680c573c5ae19808b
Matt Triano <matt.triano@gmail.com>
2017-06-02 07:26:56 -0500

    Assignment complete

--- Assignments/proj7TrianoMatt/proj7TrianoMatt.R
+++ Assignments/proj7TrianoMatt/proj7TrianoMatt.R
@@ -1,4 +1,5 @@
-# Not sure if it was intentional, but Chloresterol is actually spelled Cholesterol
+# Not sure if it was intentional, but "Chloresterol" is actually spelled Cholesterol
+setwd('C:/datasets')
 
 ChlorReads <- function(self.id, self.name, self.gender, self.ldl, self.hdl, self.trigl) {
   this <- list(id=self.id, name=self.name, gender=self.gender, ldl=self.ldl, hdl=self.hdl, trigl=self.trigl)
@@ -6,6 +7,7 @@ ChlorReads <- function(self.id, self.name, self.gender, self.ldl, self.hdl, self
   return(this)
 }
 
+# This class function appends the correct symbol to the LDL value
 get.ldl <- function(self) {
   ldl <- self$ldl
   if (ldl >= 240) {
@@ -20,6 +22,7 @@ get.ldl <- function(self) {
   return(ldl_symb)
 }
 
+# This class function appends the correct symbol to the HDL value
 get.hdl <- function(self) {
   hdl <- self$hdl
   if (hdl >= 60) {
@@ -34,6 +37,7 @@ get.hdl <- function(self) {
   return(hdl_symb)
 }
 
+# This class function appends the correct symbol to the Triglyceride value
 get.trigl <- function(self) {
   trig <- self$trigl
   if (trig >= 200) {
@@ -48,10 +52,13 @@ get.trigl <- function(self) {
   return(trig_symb)
 }
 
+# This class function produces a barplot of the Chlor data. I couldn't figure out how
+#  to force the legend to print without the grey symbol to the left of the text.
+#  Perhaps there is some overlay function or parameter I overlooked.
 plot.ChlorReads <- function(self) {
   counts <- c(self$ldl, self$hdl, self$trigl)
   if (max(counts) >= 300) {
-    max_y <- max_counts
+    max_y <- max(counts)
   } else {
     max_y <- 300
   }
@@ -61,6 +68,8 @@ plot.ChlorReads <- function(self) {
           beside=T, args.legend=list(x="topleft", bty='n', pch=NULL, pt.bg='white'))
 }
 
+# This class function prints the Chlor data formatted as in the assignment and with 
+#  the symbols for the ldl, hdl, and triglyceride values
 print.ChlorReads <- function(self) {
   cat("ID:", self$id, " Name:", self$name, " Gender:", self$gender, "\nLDL:",
       get.ldl(self), " HDL:", get.hdl(self), " Triglycerides:", get.trigl(self), "\n")
@@ -71,27 +80,70 @@ getPatientInfo <- function(self) {
   UseMethod("getPatientInfo", self)
 }
 
+# This class accessor function returns the Chlor data as desired in the assignment
 getPatientInfo.ChlorReads <- function(self) {
   data <- sprintf("%-06s %-07s %-02s", self$id, self$name, self$gender)
   cat("ID     Name    Gender\n", data, sep="")
 }
 
-# generic, Chloresterol is spelled
+# generic
 getChloresterol <- function(self) {
   UseMethod("getChloresterol", self)
 }
 
+# This class accessor function returns the Chlor data as desired in the assignment 
 getChloresterol.ChlorReads <- function(self) {
-  data <- sprintf("%-04i%02i  %-04i", self$ldl, self$hdl, self$trigl)
+  data <- sprintf("%04i%02i  %-04i", self$ldl, self$hdl, self$trigl)
   cat("LDL HDL Trigl\n",data, sep="")
 }
 
+# This takes the filename for a text file containing Chlor data and 
+# returns a list of ChlorReads objects corresponding to that data
+read_chlor_file <- function(data_filename) {
+  df <- read.table(data_filename, header=T)
+  lst <- NULL
+  for (i in 1:nrow(df)) {
+    lst[[i]] <- ChlorReads(df[i,]$id, as.character(df[i, ]$name),
+                as.character(df[i, ]$gender), df[i, ]$ldl, df[i, ]$hdl, df[i, ]$trigl)
+  }
+  return(lst)
+}
+
+# This takes a list of ChlorReads objects and prints the Chlor data
+print_chlor_data <- function(data_lst) {
+  for (item in data_lst) {
+    print(item)
+    plot(item)
+  }
+}
+
+# This takes a list of ChlorReads objects and the filename for the output PDF
+# and saves plots for all of those ChlorReads objects to that output PDF
+plot_to_pdf <- function(data_lst, pdf_filename) {
+  my_pdf <- pdf(file=pdf_filename)
+  for (item in data_lst) {
+    plot(item)
+  }
+  dev.off()
+}
+
+# The following 6 lines provide a simple test of these functions with sample data
 p1 <- ChlorReads(9876, "Virgil", "M", 248, 45, 148)
 print.ChlorReads(p1)
 print(p1)
 plot(p1)
-
 getPatientInfo(p1)
 getChloresterol(p1)
 
-#stopped the 2nd to last lectura aropund 1:30:00
+# These lines handle the bulk of section 5 of the assignment
+data_lst <- read_chlor_file('chlor-reads.txt')
+print_chlor_data(data_lst)
+plot_to_pdf(data_lst, 'TrianoMatt_Assn7_Output_PDF.pdf')
+
+
+
+
+
+
+
+
