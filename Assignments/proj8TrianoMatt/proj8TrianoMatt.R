1ddc76366d8da3a72e3f2e34f54af06a44e2572a
Matt Triano <matt.triano@gmail.com>
2017-06-03 17:04:42 -0500

    Getting Choropleths figured out.  Plotly is nice, but requires a login. I think I'm going with the choroplethrMaps build

--- Assignments/proj8TrianoMatt/proj8TrianoMatt.R
+++ Assignments/proj8TrianoMatt/proj8TrianoMatt.R
@@ -1,24 +1,83 @@
 setwd('C:/datasets')
+
+# load_plotly <- function() {
+#   If (!require(plotly)) {
+#     response <- readline("The plotly package is required to generate the maps (choropleth plots,\n
+#                        and it is not installed on your machine.  Enter 'y' if you want to install \n
+#                        the plotly package: ")
+#     if (tolower(response) == 'y') {
+#       install.packages('plotly')
+#       return(T)
+#     }
+#   } else {
+#     library(plotly)
+#   }
+# }
+library(plotly)
+library(ggplot2)
+library(choroplethr)
+library(choroplethrMaps)
 library(help=datasets)
-internet <- WWWusage
-internet
-plot(internet)
 
-fr <- freeny
-plot(fr[1])
-plot(fr[,3])
-ts(fr)
+st      <- state.x77
 
-morl <- morley
-morley
-co2 <- co2
-plot(co2)
-st <- state.x77
-st.name <- state.name
-st.name
 
-df <- cbind(c(st.name, st[,1], st[,2], st[,3], st[,4], st[,5]))
+abbs <- sapply(state.abb, tolower)
+region  <- sapply(state.name, tolower)
+region  <- data.frame(region, stringsAsFactors=F)
+st.name <- data.frame(state.name, stringsAsFactors=F)
+st.abb  <- data.frame(state.abb, stringsAsFactors=F)
+
+df <- cbind(st.abb, region, st)
 df
+
+summary(df)
+
+make_choropleth <- function(df, values) {
+  region   <- df$region
+  value    <- values
+  choro_df <- data.frame(region, value)
+  st.pleth <- state_choropleth(choro_df)
+  return(st.pleth)
+}
+df$'Income'
+temp_pleth <- make_choropleth(df, df$Income)
+plot(temp_pleth)
+
+st$state.name[1]
+df$hover <- with(df, paste(df$state.name, '<br>', 'Population', df$Population,'Income', df$Income,
+                           '<br>', 'Illiteracy', df$Illiteracy, 'HS Grad Rate', df$`HS Grad`,
+                           '<br>', 'Life Expectancy', df$`Life Exp`, 'Murder', df$Murder,
+                           '<br>', 'Area', df$Area, 'Frost', df$f))
+g <- list(
+  scope = 'usa',
+  projection = list(type = 'albers usa'),
+  showlakes = TRUE,
+  lakecolor = toRGB('white')
+)
+
+
+p <- plot_geo(df, locationmode = 'USA-states') %>%
+  add_trace(
+    z = ~Population, text = ~hover, locations = ~state.abb,
+    color = ~Population, colors = 'Purples'
+  ) %>%
+  colorbar(title = "Millions USD") %>%
+  layout(
+    title = '2011 US Agriculture Exports by State<br>(Hover for breakdown)',
+    geo = g
+  )
+
+chart_link = plotly_POST(p, filename="choropleth/ag")
+
+
+
+
+
+
+
+
+?plotly
 CO2 <- CO2
 CO2
 plot(CO2)
